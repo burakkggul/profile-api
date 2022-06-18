3083c8bb8bc6be6b8ac256ede14d95dae65f0279
Burak GÜL <burakkggul@gmail.com>
2022-06-18 10:22:25 +0300

    initial commit

---
+++ src/test/java/tr/com/burakgul/profileapi/ProfileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.burakgul.profileapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
